f2f7608df80b4cffefc02855335c40d028dd794e
Michael Harrison <michael.harrison@peak.ai>
2018-11-14 15:58:14 +0000

    Use qs for qs stuff

--- app/src/components/github-login.jsx
+++ app/src/components/github-login.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import qs from 'qs';
 import TokenManager from '../utils/token-manager';
 
 class GithubLogin extends React.Component {
   componentDidMount() {
-    const code = this.props.location.search.match(/code=([\w\d]+)/)[1];
+    const { code } = qs.parse(this.props.location.search, { ignoreQueryPrefix: true });
     axios.post('http://127.0.0.1:3000/auth/github', { code })
       .then((response) => {
         TokenManager.setToken(response.data.token);
         this.props.onLogin();
         this.props.history.push('/');
       })
-      .catch(error => {
+      .catch(() => {
         this.props.history.push('/');
       });
   }
 
   render() {
-    return <p>Reticulating Spleens...</p>;
+    return <p>Reticulating Splines...</p>;
   }
 }
 
