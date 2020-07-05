3758176a33623bbd78c500aed3c32a8aa5d0d4d9
Ian King <ian.antking@gmail.com>
2020-07-05 21:38:00 +0100

    dockerize express app

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
 
 const { auth, users } = require('./routes');
 
-dotenv.config();
 
 const app = express();
 
